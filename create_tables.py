7403867dae1deb67cd706c3dd2f9da49f3cf451a
feji08 <fengjiao.ji@uzh.ch>
2024-05-09 21:08:32 +0200

    comment out disposed files

--- create_tables.py
+++ create_tables.py
@@ -1,61 +1,61 @@
-import psycopg2
-from config import load_config
-
-def create_tables():
-    """ Create tables in the PostgreSQL database"""
-    commands = (
-
-        """
-        CREATE TABLE dataset (
-            dataset_id SERIAL PRIMARY KEY,
-            dataset_origin_name VARCHAR(50) NOT NULL,
-            dataset_n`ame VARCHAR(50) NOT NULL,
-            unique (dataset_origin_name)
-        )
-        """,
-        """ CREATE TABLE schema (
-                dataset_id INT PRIMARY KEY,
-                labelName VARCHAR(50) NOT NULL,
-                FOREIGN KEY (dataset_id)
-                REFERENCES dataset (dataset_id)
-                )
-        """,
-        """ CREATE TABLE text (
-                dataset_id INT,
-                text_id INT,
-                source_id INT,
-                text TEXT,
-                PRIMARY KEY(dataset_id, text_id),
-                FOREIGN KEY (dataset_id)
-                REFERENCES schema (dataset_id)
-                )
-        """,
-        """ CREATE TABLE label (
-                dataset_id INT,
-                text_id INT,
-                label_name VARCHAR(50),
-                label_value VARCHAR(50),
-                label_definition TEXT,
-                PRIMARY KEY(dataset_id, text_id, label_name),
-                FOREIGN KEY (dataset_id, text_id)
-                REFERENCES text (dataset_id, text_id)
-                )
-        """,
-        """ CREATE TABLE textSource(
-                source_id SERIAL PRIMARY KEY,
-                source VARCHAR(10) NOT NULL
-        )
-        """
-        )
-    try:
-        config = load_config()
-        with psycopg2.connect(**config) as conn:
-            with conn.cursor() as cur:
-                # execute the CREATE TABLE statement
-                for command in commands:
-                    cur.execute(command)
-    except (psycopg2.DatabaseError, Exception) as error:
-        print(error)
-
-if __name__ == '__main__':
-    create_tables()
+# import psycopg2
+# from config import load_config
+#
+# def create_tables():
+#     """ Create tables in the PostgreSQL database"""
+#     commands = (
+#
+#         """
+#         CREATE TABLE dataset (
+#             dataset_id SERIAL PRIMARY KEY,
+#             dataset_origin_name VARCHAR(50) NOT NULL,
+#             dataset_n`ame VARCHAR(50) NOT NULL,
+#             unique (dataset_origin_name)
+#         )
+#         """,
+#         """ CREATE TABLE schema (
+#                 dataset_id INT PRIMARY KEY,
+#                 labelName VARCHAR(50) NOT NULL,
+#                 FOREIGN KEY (dataset_id)
+#                 REFERENCES dataset (dataset_id)
+#                 )
+#         """,
+#         """ CREATE TABLE text (
+#                 dataset_id INT,
+#                 text_id INT,
+#                 source_id INT,
+#                 text TEXT,
+#                 PRIMARY KEY(dataset_id, text_id),
+#                 FOREIGN KEY (dataset_id)
+#                 REFERENCES schema (dataset_id)
+#                 )
+#         """,
+#         """ CREATE TABLE label (
+#                 dataset_id INT,
+#                 text_id INT,
+#                 label_name VARCHAR(50),
+#                 label_value VARCHAR(50),
+#                 label_definition TEXT,
+#                 PRIMARY KEY(dataset_id, text_id, label_name),
+#                 FOREIGN KEY (dataset_id, text_id)
+#                 REFERENCES text (dataset_id, text_id)
+#                 )
+#         """,
+#         """ CREATE TABLE textSource(
+#                 source_id SERIAL PRIMARY KEY,
+#                 source VARCHAR(10) NOT NULL
+#         )
+#         """
+#         )
+#     try:
+#         config = load_config()
+#         with psycopg2.connect(**config) as conn:
+#             with conn.cursor() as cur:
+#                 # execute the CREATE TABLE statement
+#                 for command in commands:
+#                     cur.execute(command)
+#     except (psycopg2.DatabaseError, Exception) as error:
+#         print(error)
+#
+# if __name__ == '__main__':
+#     create_tables()
--- insert_data.py
+++ insert_data.py
@@ -1,74 +1,74 @@
-import sqlite3
-import os
-
-import myGlobals
-conn = myGlobals.connect()
-c = conn.cursor()
-
-c.execute("SELECT name FROM sqlite_master WHERE type='table';")
-tables = c.fetchall()
-
-for table_name in tables:
-    print(table_name[0])
-#%%
-def add_dataset(file_path, dataset_name):
-    dataset_original_name = os.path.basename(file_path)
-    try:
-        c.execute("INSERT INTO dataset (datasetOriginalName, datasetName) VALUES (?, ?)",
-                  (dataset_original_name, dataset_name))
-        conn.commit()
-        print(f"Dataset {dataset_name} added successfully.")
-        return c.lastrowid
-    except sqlite3.IntegrityError as e:
-        print(f"Failed to add dataset {dataset_name}: {e}")
-
-dataset_id = add_dataset('H:\\UZH\\Master Project\\data\\edos_labelled_aggregated.csv', 'HateSpeech1')
-#%%
-def add_schema(dataset_id, label_name):
-    try:
-        c.execute("INSERT INTO schema (datasetID, labelName) VALUES (?, ?)",
-                  (dataset_id, label_name))
-        conn.commit()
-    except sqlite3.IntegrityError as e:
-        print(f"Error: {e}")
-        print("This may be due to the non-existence of the corresponding datasetID.")
-
-add_schema(dataset_id, 'Example Label')
-#%%
-def add_text(text_id, dataset_id, source_id, text_content):
-    try:
-        c.execute("INSERT INTO text (textID, datasetID, sourceID, text) VALUES (?, ?, ?, ?)",
-                  (text_id, dataset_id, source_id, text_content))
-        conn.commit()
-    except sqlite3.IntegrityError as e:
-        print(f"Error: {e}")
-        print("This may be due to a violation of a uniqueness constraint or the non-existence of the corresponding datasetID.")
-
-
-add_text(0, dataset_id, 101, 'Sample Text')
-add_text(1, dataset_id, 102, 'Sample Text 2')
-#%%
-def add_label(dataset_id, text_id, label_name, label_value, label_definition):
-    try:
-        c.execute("INSERT INTO label (datasetID, textID, labelName, labelValue, labelDefinition) VALUES (?, ?, ?, ?, ?)",
-                  (dataset_id, text_id, label_name, label_value, label_definition))
-        conn.commit()
-    except sqlite3.IntegrityError as e:
-        print(f"Error: {e}")
-        print("This may be due to a violation of uniqueness constraints, foreign key constraints, or the non-existence of the corresponding datasetID and textID.")
-
-
-add_label(dataset_id, 0, 'Category', 'Education', 'Defines the type of education topic discussed.')
-#%%
-def add_text_source(source_name):
-    try:
-        c.execute("INSERT INTO textSource (source) VALUES (?)",
-                  (source_name,))
-        conn.commit()
-    except sqlite3.IntegrityError as e:
-        print(f"Error: {e}")
-        print("An error occurred while adding data.")
-
-add_text_source('Wikipedia Article')
-#%%
-conn.close()
+# import sqlite3
+# import os
+#
+# import myGlobals
+# conn = myGlobals.connect()
+# c = conn.cursor()
+#
+# c.execute("SELECT name FROM sqlite_master WHERE type='table';")
+# tables = c.fetchall()
+#
+# for table_name in tables:
+#     print(table_name[0])
+# #%%
+# def add_dataset(file_path, dataset_name):
+#     dataset_original_name = os.path.basename(file_path)
+#     try:
+#         c.execute("INSERT INTO dataset (datasetOriginalName, datasetName) VALUES (?, ?)",
+#                   (dataset_original_name, dataset_name))
+#         conn.commit()
+#         print(f"Dataset {dataset_name} added successfully.")
+#         return c.lastrowid
+#     except sqlite3.IntegrityError as e:
+#         print(f"Failed to add dataset {dataset_name}: {e}")
+#
+# dataset_id = add_dataset('H:\\UZH\\Master Project\\data\\edos_labelled_aggregated.csv', 'HateSpeech1')
+# #%%
+# def add_schema(dataset_id, label_name):
+#     try:
+#         c.execute("INSERT INTO schema (datasetID, labelName) VALUES (?, ?)",
+#                   (dataset_id, label_name))
+#         conn.commit()
+#     except sqlite3.IntegrityError as e:
+#         print(f"Error: {e}")
+#         print("This may be due to the non-existence of the corresponding datasetID.")
+#
+# add_schema(dataset_id, 'Example Label')
+# #%%
+# def add_text(text_id, dataset_id, source_id, text_content):
+#     try:
+#         c.execute("INSERT INTO text (textID, datasetID, sourceID, text) VALUES (?, ?, ?, ?)",
+#                   (text_id, dataset_id, source_id, text_content))
+#         conn.commit()
+#     except sqlite3.IntegrityError as e:
+#         print(f"Error: {e}")
+#         print("This may be due to a violation of a uniqueness constraint or the non-existence of the corresponding datasetID.")
+#
+#
+# add_text(0, dataset_id, 101, 'Sample Text')
+# add_text(1, dataset_id, 102, 'Sample Text 2')
+# #%%
+# def add_label(dataset_id, text_id, label_name, label_value, label_definition):
+#     try:
+#         c.execute("INSERT INTO label (datasetID, textID, labelName, labelValue, labelDefinition) VALUES (?, ?, ?, ?, ?)",
+#                   (dataset_id, text_id, label_name, label_value, label_definition))
+#         conn.commit()
+#     except sqlite3.IntegrityError as e:
+#         print(f"Error: {e}")
+#         print("This may be due to a violation of uniqueness constraints, foreign key constraints, or the non-existence of the corresponding datasetID and textID.")
+#
+#
+# add_label(dataset_id, 0, 'Category', 'Education', 'Defines the type of education topic discussed.')
+# #%%
+# def add_text_source(source_name):
+#     try:
+#         c.execute("INSERT INTO textSource (source) VALUES (?)",
+#                   (source_name,))
+#         conn.commit()
+#     except sqlite3.IntegrityError as e:
+#         print(f"Error: {e}")
+#         print("An error occurred while adding data.")
+#
+# add_text_source('Wikipedia Article')
+# #%%
+# conn.close()
