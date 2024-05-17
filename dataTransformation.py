2b4cdcd258a5e692c292c5cca4432309198d769a
feji08 <fengjiao.ji@uzh.ch>
2024-05-18 00:43:05 +0200

    fix format_text bug; add logic to deal with source/language columns

--- dataTransformation.py
+++ dataTransformation.py
@@ -2,18 +2,25 @@ import pandas as pd
 
 class DataFrameConverter:
     def __init__(self, config_df, dataframe, connection):
+        self.file_idx = 0 #use one instance for separate files
         self.df = dataframe
         self.conn = connection
         self.formatted_df = {}
         self.config = config_df
-
-    def format_for_sql(self):
+        self.source_cache = {}
+        self.language_cache = {}
+        self.max_source_id = self.__get_rowid('text_source')
+        self.max_language_id = self.__get_rowid('language')
+    # enable append mode to use one instance for separate files
+    def format_for_sql(self, append = False):
         self.__format_dataset()
         self.__format_schema()
         self.__format_source()
         self.__format_language()
         self.__format_text()
         self.__format_label()
+        if append:
+            self.file_idx += 1
         return self.formatted_df
     def __format_dataset(self):
         self.dataset_id = self.__get_rowid("dataset")+1
@@ -33,28 +40,60 @@ class DataFrameConverter:
         table_schema = pd.DataFrame(data)
         self.formatted_df['schema'] = table_schema
     def __format_source(self):
-        self.source_id = self.__get_rowid("text_source")+1
-        text_source_data = {
-            "source_id": [self.source_id],
-            'source': [self.config['source']]
-        }
+        if self.config['source'].startswith('@'):
+            source = self.config['source'][1:]
+            source_id = self.__search_source_rowid(source)
+            text_source_data = {
+                "source_id": [source_id],
+                'source': [source]
+            }
+        # for a source col, fetch row_id by row
+        else:
+            source_col =self.df[self.config['source']]
+            text_source_data = {
+                "source_id": [],
+                'source': []
+            }
+            for source in source_col:
+                source_id = self.__search_source_rowid(source)
+                text_source_data['source_id'].append(source_id)
+                text_source_data['source'].append(source)
         text_source_df = pd.DataFrame(text_source_data)
         self.formatted_df['text_source'] = text_source_df
     def __format_language(self):
-        self.language_id = self.__get_rowid("language")+1
-        language_data = {
-            'language': [self.config['language']]
-        }
+        if self.config['language'].startswith('@'):
+            language = self.config['source'][1:]
+            language_id = self.__search_language_rowid(language)
+            language_data = {
+                "language_id": [language_id],
+                'language': [language]
+            }
+        else:
+            language_col =self.df[self.config['language']]
+            language_data = {
+                "language_id": [],
+                'language': []
+            }
+            for language in language_col:
+                language_id = self.__search_language_rowid(language)
+                language_data['language_id'].append(language_id)
+                language_data['language'].append(language)
         language_df = pd.DataFrame(language_data)
         self.formatted_df['language'] = language_df
     def __format_text(self):
         self.df["dataset_id"] = self.dataset_id
-        self.df.rename(columns={'id': 'text_id',
-                    'tweet':'text'
+        text_col = self.config['text']
+        self.df.rename(columns={
+                    # 'id': 'text_id',
+                    'tweet':text_col
                     }, inplace=True)
-        table_text = self.df[['dataset_id', 'text_id', 'text']].drop_duplicates()
-        table_text['source_id'] = self.source_id
-        table_text['language_id'] = self.language_id
+        # problem is that for separate files in the same dataset, it cannot be used
+        # maybe can be solved by fetching the MAX text_id for this dataset
+        # converter need to "remember" the dataset
+        self.df['text_id'] = self.df.index + 1
+        table_text = self.df[['dataset_id', 'text_id', text_col]].drop_duplicates()
+        table_text['source_id'] = self.max_source_id
+        table_text['language_id'] = self.max_language_id
         self.formatted_df['text'] = table_text
     def __format_label(self):
         table_label = pd.DataFrame(columns = ['dataset_id', 'text_id', 'label_name', 'label_value'])
@@ -68,12 +107,46 @@ class DataFrameConverter:
             temp_df = pd.DataFrame(label_value_pair)
             table_label = pd.concat([table_label, temp_df], axis=0, ignore_index=True)
         self.formatted_df['label'] = table_label
+    def __search_source_rowid(self, source):
+        # first check the dic, which consists of
+        # #1.existing source-id pairs that have been queried
+        # #2.non-existing pairs that have been created in the dic
+        if source in self.source_cache:
+            return self.source_cache[source]
+        # then try to query it form the db
+        c = self.conn.cursor()
+        c.execute("SELECT source_id FROM text_source WHERE source = ?", (source,))
+        source_id = c.fetchone()
+        if source_id is not None:
+            self.source_cache[source] = source_id[0]
+            return source_id[0]
+        # create new source_id
+        else:
+            self.max_source_id += 1
+            self.source_cache[source] = self.max_source_id
+            return self.max_source_id
+    def __search_language_rowid(self, language):
+        # first check the dic
+        if language in self.language_cache:
+            return self.language_cache[language]
+        # then try to query it form the db
+        c = self.conn.cursor()
+        c.execute("SELECT language_id FROM language WHERE language = ?", (language,))
+        language_id = c.fetchone()
+        if language_id is not None:
+            self.language_cache[language] = language_id[0]
+            return language_id[0]
+        # create new
+        else:
+            self.max_language_id += 1
+            self.language_cache[language] = self.max_language_id
+            return self.max_language_id
     def __get_rowid(self, table_name):
         '''Get the ID of the last inserted record in the specified table'''
         c = self.conn.cursor()
         c.execute("SELECT MAX(rowid) FROM {}".format(table_name))
         last_inserted_id = c.fetchone()[0]
-        return last_inserted_id
+        return last_inserted_id if last_inserted_id is not None else 0
 
 
 # ### Configuration
--- main.py
+++ main.py
@@ -2,20 +2,21 @@ import pandas as pd
 from dataTransformation import DataFrameConverter
 from sqlite_setup import create_tables
 from read_data import read_table, read_all_tables
+from delete_data import clear_tables
 import myGlobals
 conn = myGlobals.connect()
+clear_tables(conn)
 create_tables(conn)
-read_all_tables(conn)
 
 # df = pd.read_csv("./OLID_2019/olid-training-v1.0.tsv", sep="\t")
 # converter = DataFrameConverter(df, conn)
 # ## dictionary key:table name; value:df
 # formatted_df = converter.format_for_sql()
-# def insert_data(conn, df, table_name):
-#     try:
-#         df.to_sql(table_name, conn, if_exists='append', index=False)
-#     except Exception as e:
-#         print(e)
+def insert_data(conn, df, table_name):
+    try:
+        df.to_sql(table_name, conn, if_exists='append', index=False)
+    except Exception as e:
+        print(e)
 
 # for k,v in formatted_df.items():
 #     insert_data(conn, v, k)
@@ -23,7 +24,6 @@ read_all_tables(conn)
 
 # # conn = myGlobals.connect()
 # read_all_tables(conn)
-
 
 from readConfig import final_config
 from readConfig import read_dataframe
@@ -33,15 +33,26 @@ data_folder = "./data"
 config = final_config(config_path, data_folder)
 for index, row in config.iterrows():
     file_names = row['dataset_file_name'].split(', ')
-    
+
     for file_name in file_names:
-        full_path = data_folder + '/' + file_name.strip()  
-        try:
-            df = read_dataframe(full_path)
-            converter = DataFrameConverter(row, df, conn)
-            formatted_df = converter.format_for_sql()
-            print(formatted_df)
-        except FileNotFoundError:
-            print(f"File not found: {full_path}")
-        except Exception as e:
-            print(f"An error occurred: {e}")
+        full_path = data_folder + '/' + file_name.strip()
+        df = read_dataframe(full_path)
+        converter = DataFrameConverter(row, df, conn)
+        formatted_df = converter.format_for_sql()
+        for table_name, df in formatted_df.items():
+            print(f"TABLE {table_name}:\n{df}")
+            print("\n")
+        # for table_name, df in formatted_df.items():
+            # insert_data(conn, df, table_name)
+            # conn.commit()
+        # try:
+        #     df = read_dataframe(full_path)
+        #     converter = DataFrameConverter(row, df, conn)
+        #     formatted_df = converter.format_for_sql()
+        #     print(formatted_df)
+        # except FileNotFoundError:
+        #     print(f"File not found: {full_path}")
+        # except Exception as e:
+        #     print(f"An error occurred: {e}")
+
+read_all_tables(conn)
