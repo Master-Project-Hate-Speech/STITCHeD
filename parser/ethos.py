ca2468b68f3da0c78f7563c0a6912994abf94724
Linsey <mshk8638@gmail.com>
2024-06-27 19:25:22 +0200

    add german datasets

--- parser/ethos.py
+++ parser/ethos.py
@@ -3,7 +3,8 @@ original_file_path = '.\\original_data\\'
 file_path = '.\\data\\'
 
 df1 = pd.read_csv(original_file_path + 'Ethos_Dataset_Binary.csv', sep=';')
-df1.to_csv(file_path + 'Ethos_Dataset_Binary.csv', index = False)
 
 df2 = pd.read_csv(original_file_path + 'Ethos_Dataset_Multi_Label.csv', sep=';')
-df2.to_csv(file_path + 'Ethos_Dataset_Multi_Label.csv', index = False)
+merged_df = pd.merge(df1, df2, on='comment', how='left')
+
+merged_df.to_csv(file_path + 'Ethos_merged.csv', index = False)
