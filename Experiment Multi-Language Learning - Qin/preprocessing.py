be4703c79137663ade6352295a3637b9cf878b80
Linsey <mshk8638@gmail.com>
2024-09-15 18:49:55 +0200

    update multilanguage experiment code

--- Experiment Multi-Language Learning - Qin/preprocessing.py
+++ Experiment Multi-Language Learning - Qin/preprocessing.py
@@ -1,14 +1,15 @@
 import pandas as pd
-path = path
+path = "path"
 # df_en = pd.read_csv(path + "Dynamically Generated Hate Dataset v0.2.3.csv")
 # df_de = pd.read_csv(path + "gahd.csv")
-df = pd.read_csv(path + "multilingual_all_data.csv")
+df = pd.read_csv(path + "multilingual_all_data.csv") # Generate using pipeline
 #%%
 df = df[df['label_name'] == 'label']
 df['label_value'] = df['label_value'].replace({'hate': 1, 'nothate': 0})
 #%%
-from Sampler import balanced_downsampling
+from sampling import balanced_downsampling
 resampled_df = balanced_downsampling(df, 'label_value', second_col='language')
+resampled_df['label_value'] = resampled_df['label_value'].astype(int)
 
 label_value_dist = len(resampled_df[resampled_df['label_value'] == 1]) / len(resampled_df)
 language_dist_eng = len(resampled_df[resampled_df['language'] == 'eng']) / len(resampled_df)
@@ -21,4 +22,5 @@ print(f"Distribution of language == 'ger': {language_dist_ger}")
 df_en = resampled_df[resampled_df['language'] == 'eng']
 df_de = resampled_df[resampled_df['language'] == 'ger']
 
-# resampled_df.to_csv("resampled_multilingual_all_data.csv", index = False)
+resampled_df.to_csv("FINAL_resampled_multilingual_all_data.csv", index = False)
+
